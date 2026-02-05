ea4985b31fde3cf62e80cbcd20d9c10c7906fd34
Arpit Khatua <arpitkhatua73@gmail.com>
2026-02-05 06:46:21 +0530

    first commit

---
+++ Arithmatic_opr.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4;
+    int b=a++ + ++a*2;
+    cout<<b<<endl;
+    int x=1;
+    int y=x++ + x++ + --x;
+    cout<<y;
+    return 0;
+}
---
+++ local_global.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+// int x; //Golbal vaariable which defult value is 0.
+int x=10;
+
+int main(){
+    // int y; //Local variable which default value is garbage value.
+    // int y=5;
+    int x=20;
+    cout<<x<<endl;
+    cout<<x<<endl;
+    return 0;
+}
